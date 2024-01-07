87d3534694320650864e8e4a085a15ac4c8878e0
Ola2524 <ola.mohamed.mmb@gmail.com>
2024-01-07 14:36:56 +0200

    edit data

--- js/script.js
+++ js/script.js
@@ -1,37 +1,14 @@
-import suar from "./suar.js";
+let pages = document.getElementById("moshaf-pages")
+// const parsesvg = new DOMParser().parseFromString(XMLDocument, "text/xml");
+// console.log(parsesvg);
+// for (let index = 1; index < 3; index++) {
+    // pages.innerHTML += `<img src="../images/00${index}.svg">`    
+//     pages.innerHTML += `<object id="sura" data="../images/00${index}.svg" type="image/svg+xml"></object>`
+//     let sura = document.getElementById("sura")
+//     console.log(sura.value);
+// }
 
+
+pages.innerHTML += `<object id="sura" data="../images/001.svg" type="image/svg+xml"></object>`
 let sura = document.getElementById("sura")
-
-for (let index = 0; index < 7; index++) {
-
-    sura.innerHTML += `
-    <div id="ayah" class="ayah">
-        <div class="ayah-text">${suar[index].aya_text_emlaey}</div>
-        <div class="separator">
-            <span>${suar[index].aya_no}</span>
-            <div class="separator-img">
-                <img src="../images/separator.png">
-            </div>
-        </div>
-    </div>
-    `
-
-    // let separator = document.createElement("div");
-    // let separator_text = document.createElement("span");
-    // separator.className = "separator";
-    // let separator_img = document.createElement("img");
-    // separator_img.src = "../images/separator.png";
-    // separator_img.className = "separator-img"
-
-    // let ayah = document.createElement("span");
-    // ayah.id = suar[index].id
-    // ayah.className = "ayah"
-    // ayah.innerText = `${suar[index].aya_text_emlaey} `
-    // separator_text.innerText = suar[index].aya_no
-    // separator.appendChild(separator_text)
-    // separator.appendChild(separator_img)
-    // ayah.appendChild(separator)
-    // first_page.appendChild(ayah);
-}
-
-// ${suar[index].aya_text_emlaey}
+console.log(sura);
