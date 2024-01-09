3f094a53b181fe0c92ecf9571a35c44f95144783
Ola2524 <ola.mohamed.mmb@gmail.com>
2024-01-09 04:56:15 +0200

    change design

--- js/script.js
+++ js/script.js
@@ -1,43 +1,21 @@
-let pages = document.getElementById("moshaf-pages")
+let hezb = document.getElementById("hezb")
+let suar = document.getElementById("suar")
+let page_num = document.getElementById("page-num")
+let suar_filter = document.querySelector(".suar-filter")
+let hezb_filter = document.querySelector(".hezb-filter")
 
-// pages.innerHTML += `
-// <div class="border">
-// <img src="../images/border.svg"/>
-// </div>
-// <object id="sura" data="../images/003.svg" type="image/svg+xml"></object>`
-var page = 1
-pages.innerHTML += `<object id="sura" data="../images/${page}.svg" type="image/svg+xml"></object>`
-
-window.addEventListener("load", function () {
-    let svgObject = document.getElementById('sura').contentDocument;
-    let ayah = svgObject.querySelectorAll(".ayahPolygon");
-
-    for (let index = 0; index < ayah.length; index++) {
-
-        ayah[index].addEventListener("mouseover", function () {
-            ayah[index].setAttribute("fill-opacity", 0.05);
-            ayah[index].style.cursor = "pointer"
-        })
-
-        ayah[index].addEventListener("mouseout", function () {
-            ayah[index].setAttribute("fill-opacity", 0);
-        })
-    }
+suar.addEventListener("click", function() {
+    suar_filter.style.display = "flex"
 });
 
-let nextBtn = document.getElementById("next");
-let prevBtn = document.getElementById("prev");
-
-nextBtn.addEventListener("click", function () {
-    if (page < 10) {
-        page++
-        pages.innerHTML = `<object id="sura" data="../images/${page}.svg" type="image/svg+xml"></object>`
-    }
+hezb.addEventListener("click", function() {
+    hezb_filter.style.display = "flex"
 });
 
-prevBtn.addEventListener("click", function () {
-    if (page > 0) {
-        page--
-        pages.innerHTML = `<object id="sura" data="../images/${page}.svg" type="image/svg+xml"></object>`
-    }
-});
+function closeSuarFilter(){
+    suar_filter.style.display = "none"
+}
+
+function closeHezbFilter(){
+    hezb_filter.style.display = "none"
+}
